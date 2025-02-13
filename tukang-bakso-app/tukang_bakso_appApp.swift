2b27b109c07034d303bfd4e3fd243aa1a6bd3e37
Oka <oka_uliandana@telkomsel.co.id>
2025-02-13 17:07:36 +0700

    Initial Commit

---
+++ tukang-bakso-app/tukang_bakso_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tukang_bakso_appApp.swift
+//  tukang-bakso-app
+//
+//  Created by oka_uliandana on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct tukang_bakso_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
